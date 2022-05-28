e961eacf01ebf8970dd784faf6893357df5d5f66
darien92 <64978953+darien92@users.noreply.github.com>
2022-05-27 22:19:41 -0400

    Store and Middleware added

--- core/src/main/java/com/darien/core/viewmodel/BaseViewModel.kt
+++ core/src/main/java/com/darien/core/viewmodel/BaseViewModel.kt
@@ -4,19 +4,15 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.darien.core.redux.Action
-import com.darien.core.redux.Reducer
 import com.darien.core.redux.State
+import com.darien.core.redux.Store
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 
 open class BaseViewModel<S: State, A: Action> (
-    private val reducer: Reducer<S, A>,
-    initialState: S,
+    private val store: Store<S, A>,
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow(initialState)
-    val uiState = _uiState.asStateFlow()
+    val uiState = store.state
 
     protected val viewModelSafeScope by lazy {
         viewModelScope + coroutineExceptionHandler
@@ -28,6 +24,9 @@ open class BaseViewModel<S: State, A: Action> (
         }
     }
 
+    /**
+     * Allows to handle coroutines errors
+     */
     open fun handleError(exception: Throwable) {
         Log.e("BaseViewModel",
             "CoroutineExceptionHandler: ${exception.localizedMessage}",
@@ -35,15 +34,9 @@ open class BaseViewModel<S: State, A: Action> (
         )
     }
 
-    protected fun stopJob() {
-        if(viewModelSafeScope.isActive) {
-            viewModelSafeScope.cancel()
-        }
-    }
-
-    protected fun handleAction(action: A) {
+    open fun handleAction(action: A) {
         viewModelSafeScope.launch {
-            _uiState.value = reducer.reduce(_uiState.value, action)
+            store.dispatch(action)
         }
     }
 }
