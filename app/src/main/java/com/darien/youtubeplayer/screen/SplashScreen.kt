c0d5458bcce25f35f678eeeae0c293ba8054c01a
darien92 <64978953+darien92@users.noreply.github.com>
2022-05-27 23:02:13 -0400

    SplashScreen added

---
+++ app/src/main/java/com/darien/youtubeplayer/screen/SplashScreen.kt
@@ -0,0 +1,40 @@
+package com.darien.youtubeplayer.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.darien.youtubeplayer.R
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier
+                .height(180.dp)
+                .width(250.dp),
+            painter = painterResource(id = R.drawable.ic_player_icon),
+            contentDescription = stringResource(
+                id = R.string.youtube_player_icon_description
+            ),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    MaterialTheme {
+        SplashScreen()
+    }
+}
---
+++ search/ui/src/main/java/com/darien/ui/SearchScreen.kt
@@ -0,0 +1,12 @@
+package com.darien.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SearchScreen(
+    navController: NavController
+) {
+    Text(text = "Search Screen")
+}
